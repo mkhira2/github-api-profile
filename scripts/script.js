af7c8a74fcce7ae5451e1abc20ab37cba39f9153
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-22 15:35:41 -0600

    added font awesome icons

--- scripts/script.js
+++ scripts/script.js
@@ -21,13 +21,14 @@ var responseHandler = function(profileObj) {
     var profileStr = ''
     profileStr += '<img class="profilePic" src=' + '"' + profilePic + '">'
     profileStr += '<h1 class="name">' + name + '</h1>'
-    profileStr += '<p class="username">' + username + '</p>'
+    profileStr += '<p class="username">' + '<i class="fa fa-id-card-o" aria-hidden="true"></i>' + ' ' + username + '</p>'
     profileStr += '<p class="bio">' + bio + '</p>'
-    // ADD ME ---> <button type="button">FOLLOW</button>
+    profileStr += '<button type="button" class="follow">FOLLOW</button>'
+    profileStr += '<p class="badUser">Block or Report User</p>'
     profileStr += '<hr>'
-    profileStr += '<p class="location">' + locationEl + '</p>'
-    profileStr += '<p class="website"><a href="' + website + '">' + website + '</a></p>'
-    profileStr += '<p class="blog"><a href="' + blog + '">' + blog + '</a></p>'
+    profileStr += '<p class="location">' + '<i class="fa fa-map-marker" aria-hidden="true"></i>' + ' ' + locationEl + '</p>'
+    profileStr += '<p class="website">' + '<i class="fa fa-github" aria-hidden="true"></i>' + ' ' + '<a href="' + website + '">' + website + '</a></p>'
+    profileStr += '<p class="blog">' + '<i class="fa fa-external-link" aria-hidden="true"></i>' + ' ' + '<a href="' + blog + '">' + blog + '</a></p>'
 
     // getting built string for profile
     profile_column.innerHTML = profileStr
@@ -47,7 +48,8 @@ var responseHandlerTwo = function(repoArray) {
         var repoURL = repoArray[i].html_url
         repoNode.innerHTML += '<h1><a class="repo_title" href="' + repoURL + '">' + currentObj.name + '</a></h1>'
         repoNode.innerHTML += '<p class="repo_description">' + currentObj.description + '</p>'
-        repoNode.innerHTML += '<p class="repo_language"><ul><li><span>' + currentObj.language + '</span></li></ul></p><hr></div><br />'
+        repoNode.innerHTML += '<p class="repo_language"><ul><li><span>' + currentObj.language + '</span></li></ul></p>'
+        repoNode.innerHTML += '<hr></div><br />'
         repo_column.appendChild(repoNode)
     }
 }
