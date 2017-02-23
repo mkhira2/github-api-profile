de393ff45be0ab2fa9258c3d6824c75a96c6d940
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-22 21:20:25 -0600

    added search feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
-// define global variables for apis and columns
+//--------------------------------------------------
+// GLOBAL VARIABLES
+//--------------------------------------------------
+
 var profileURL = 'https://api.github.com/users/mkhira2'
 var repoURL = 'https://api.github.com/users/mkhira2/repos'
+var baseURL = 'https://api.github.com/users/'
 var profile_column = document.querySelector('.profile_column')
 var repo_column = document.querySelector('.repo_column')
+var searchNode = document.querySelector('.searchBar')
 
-// profile promise
-var promise = $.getJSON(profileURL)
+
+//--------------------------------------------------
+// PROFILE SIDE
+//--------------------------------------------------
+
+var profilePromise = $.getJSON(profileURL)
 
 // this function will run once the profile promise is fulfilled
 var responseHandler = function(profileObj) {
@@ -17,7 +26,6 @@ var responseHandler = function(profileObj) {
     var website = profileObj.html_url
     var blog = profileObj.blog
 
-    // building string for profile
     var profileStr = ''
     profileStr += '<img class="profilePic" src=' + '"' + profilePic + '">'
     profileStr += '<h1 class="name">' + name + '</h1>'
@@ -30,29 +38,54 @@ var responseHandler = function(profileObj) {
     profileStr += '<p class="website">' + '<i class="fa fa-github" aria-hidden="true"></i>' + ' ' + '<a href="' + website + '">' + website + '</a></p>'
     profileStr += '<p class="blog">' + '<i class="fa fa-external-link" aria-hidden="true"></i>' + ' ' + '<a href="' + blog + '">' + blog + '</a></p>'
 
-    // getting built string for profile
     profile_column.innerHTML = profileStr
 }
 
-// once promise is fulfilled, run responseHandler function
-promise.then(responseHandler)
+// make promise hold onto responseHandler function
+profilePromise.then(responseHandler)
 
-// repo promise
-var promiseTwo = $.getJSON(repoURL)
+//--------------------------------------------------
+// REPO SIDE
+//--------------------------------------------------
+
+var repoPromise = $.getJSON(repoURL)
 
 // this function will run once the repo promise is fulfilled
 var responseHandlerTwo = function(repoArray) {
     for (var i = 0; i < repoArray.length; i++) {
         var repoNode = document.createElement('div')
         var currentObj = repoArray[i]
-        var repoURL = repoArray[i].html_url
-        repoNode.innerHTML += '<h1><a class="repo_title" href="' + repoURL + '">' + currentObj.name + '</a></h1>'
+        repoNode.innerHTML += '<h1><a class="repo_title" href="' + currentObj.html_url + '">' + currentObj.name + '</a></h1>'
         repoNode.innerHTML += '<p class="repo_description">' + currentObj.description + '</p>'
         repoNode.innerHTML += '<p class="repo_language"><ul><li><span>' + currentObj.language + '</span></li></ul></p>'
         repoNode.innerHTML += '<hr></div><br />'
         repo_column.appendChild(repoNode)
     }
+
 }
 
-// once promise is fulfilled, run responseHandlerTwo function
-promiseTwo.then(responseHandlerTwo)
+// make promise hold onto responseHandler function
+repoPromise.then(responseHandlerTwo)
+
+//--------------------------------------------------
+// SEARCH FEATURE
+//--------------------------------------------------
+
+var searchUser = function(eventObj) {
+    if (eventObj.keyCode == 13) {
+        repo_column.innerHTML = ''
+        var username = eventObj.target.value
+
+        var searchProfileURL = baseURL + username
+        var profileSearchPromise = $.getJSON(searchProfileURL)
+        profileSearchPromise.then(responseHandler)
+
+        var searchRepoURL = baseURL + username + '/repos'
+        var repoSearchPromise = $.getJSON(searchRepoURL)
+        repoSearchPromise.then(responseHandlerTwo)
+
+        eventObj.target.value = ''
+    }
+}
+
+searchNode.addEventListener('keydown', searchUser)
